7594a6c325f01738b55a78b903b0966a5adbe633
FabianMontes <fmontes@unal.edu.co>
2023-04-14 20:41:18 -0500

    g funcional

--- src/components/CreateCourse.vue
+++ src/components/CreateCourse.vue
@@ -13,7 +13,7 @@
   
           <div class="form-group col-12">
             <label class="custom-label col-md-3 display" for="horas">Horas</label>
-            <input id="horas" class="form-control col-12 col-sm-10 col-md-7 offset-sm-1" type="text" placeholder="Horas" v-model="courseDurationHours" required/>
+            <input id="horas" class="form-control col-12 col-sm-10 col-md-7 offset-sm-1" type="text" placeholder="Horas" v-model="durationHours" required/>
               
           </div>
           <div class="col-12 mb-3">
@@ -29,33 +29,39 @@
 
 <script>
 import axios from 'axios';
+import { getAuthenticationToken } from "@/dataStorage";
+
 
 export default {
+  name: "CreateCourse",
   data() {
     return {
-      names: '',
-      horas: null,
+      courseName: '',
+      durationHours: null,
     };
   },
   methods: {
-    crearCurso() {
+    associate(event) {
       // Validación básica de formulario
-      if (!this.names || !this.horas) {
+      
+      if (!this.courseName || !this.durationHours) {
         alert('Por favor, complete todos los campos.');
         return;
       }
-
       // Llamada a la API de Spring para crear el curso
       const nuevoCurso = {
-        nombre: this.names,
-        duracion: this.horas,
+        courseName: this.courseName,
+        durationHours: this.durationHours,
       };
-      axios.post('http://localhost:9090/profesor/crear-curso', nuevoCurso) // Cambiar la URL según la configuración de tu backend de Spring
+
+  
+
+      axios.post('http://localhost:9090/profesor/crear-curso', nuevoCurso,{ params: { access_token: getAuthenticationToken() }}) // Cambiar la URL según la configuración de tu backend de Spring
         .then(response => {
           console.log('Nuevo Curso creado:', response.data);
           // Reiniciar los campos del formulario
-          this.names = '';
-          this.horas = null;
+          this.courseName = '';
+          this.durationHours = null;
         })
         .catch(error => {
           console.error('Error al crear el curso:', error);
